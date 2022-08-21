fa2aad942ce8edcead2494098b990f1ea5385c62
KokiYamada <nino02000@icloud.com>
2022-08-21 13:09:56 +0000

    data structures

---
+++ spiral_book/lesson04/libraryStackSample.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	stack<int> S;
+
+	S.push(3);
+	S.push(7);
+	S.push(1);
+	cout << S.size() << " ";  // stack size = 3
+
+	cout << S.top() << " ";  // 1
+	S.pop();
+
+	cout << S.top() << " ";  // 7
+	S.pop();
+
+	cout << S.top() << " ";  //3
+
+	S.push(5);
+
+	cout << S.top() << " ";  // 5
+	S.pop();
+
+	cout << S.top() << endl;
+
+	return 0;
+}
